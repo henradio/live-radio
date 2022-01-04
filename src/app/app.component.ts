771b50267d2fdb12dcae4583277b3f98a5f0019c
Hen.radio <dev@hen.radio>
2022-01-04 15:45:27 +0000

    logo link to hen.radio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'live-radio';
   ngAfterViewInit() {
-    console.error('VIEW INIT')
+    //console.error('VIEW INIT')
   }
 
 }
